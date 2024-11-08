77cacf3984b73d097c8875593d527fae502a8e97
slamaniabdelhafid <st130302@student.spbu.ru>
2024-11-08 14:02:35 +0300

    homeassignment04

--- HomeAssignment4/Car.h
+++ HomeAssignment4/Car.h
@@ -1,3 +1,4 @@
+/*slamani abdelhafid.  group 24.b83 . st130302@student.spbu.ru*/
 #ifndef CAR_H
 #define CAR_H
 
--- HomeAssignment4/Vehicle.h
+++ HomeAssignment4/Vehicle.h
@@ -1,3 +1,4 @@
+/*slamani abdelhafid.  group 24.b83 . st130302@student.spbu.ru*/
 #ifndef VEHICLE_H
 #define VEHICLE_H
 
